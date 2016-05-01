4af814e6276d79c49b1d1540238283f76a8b68a5
zocke1r <zocke1r@gmail.com>
2016-05-01 16:30:54 +0200

    Added barebone for Productivity Modifications

--- utility/utility.lua
+++ utility/utility.lua
@@ -15,3 +15,20 @@ function addTechnologyRecipes (dicitionary)
 		 addTechnologyRecipe(key,value)
 	end
 end
+function addProductivityLimitation(intermediate)
+  for i, module in pairs(data.raw.module) do
+    if module.limitation then
+      table.insert(module.limitation, intermediate)
+    end
+  end
+end
+
+function addProductivityLimitations(intermediates)
+  for i, module in pairs(data.raw.module) do
+    if module.limitation then
+      for j, intermediate in pairs(intermediates) do
+        table.insert(module.limitation, intermediate)
+      end
+    end
+  end
+end
